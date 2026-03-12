037d397015ae1db07e35722f719bc82fde098e66
sameer tahir <sameertahir024@gmail.com>
2026-03-12 10:50:47 +0500

    wee add the ARRAY

---
+++ array.js
@@ -0,0 +1,31 @@
+// return true is the element is appear more than once
+
+// let nums = [1, 2, 3, 2];
+
+// // Output: true;
+
+// // solution//
+// function checkBublicate(nums) {
+//    let seen = new Set();
+//    for (let n of nums) {
+//       if (seen.has(n)) return true;
+//       seen.add(n);
+//    }
+//    return false;
+// }
+
+// console.log(checkBublicate(nums));
+
+// let s = "racecar";
+// let t = "carrace";
+
+// // Output: true;
+// // solution
+
+// function checkAnagram(s, t) {
+//    let one = s.split("").sort().join(" ");
+//    let two = t.split("").sort().join(" ");
+//    return one === two;
+// }
+
+// console.log(checkAnagram(s, t));
