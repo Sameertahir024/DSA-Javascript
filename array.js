ebd8cb030d48d00e5d628c3c6c2111fb415dacc4
sameer tahir <sameertahir024@gmail.com>
2026-03-12 11:33:28 +0500

    add new question

--- array.js
+++ array.js
@@ -29,3 +29,40 @@
 // }
 
 // console.log(checkAnagram(s, t));
+//
+//
+// let nums = [3, 4, 5, 6];
+// let target = 7;
+
+// // Output: [0,1]
+// //
+// function twoSun(nums, target) {
+//    for (let i = 0; i < nums.length; i++) {
+//       for (let j = i + 1; j < nums.length; j++) {
+//          if (nums[i] + nums[j] === target) return [nums[i], nums[j]];
+//       }
+//    }
+
+//    return " no target find";
+// }
+
+// console.log(twoSun(nums, target));
+//
+//
+let strs = ["act", "pots", "tops", "cat", "stop", "hat"];
+
+// Output: [["hat"],["act", "cat"],["stop", "pots", "tops"]]
+//
+
+function makeGroupAnagram(strs) {
+   let group = {};
+   for (let s of strs) {
+      const sort = s.split("").sort().join("");
+      if (!group[sort]) {
+         group[sort] = [];
+      }
+      group[sort].push(sort);
+   }
+   return Object.values(group);
+}
+console.log(makeGroupAnagram(strs));
